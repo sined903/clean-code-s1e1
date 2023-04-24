e4fffe2738e1c2a364899e618b4bf12bcfe44053
Denis Kulikov <9933denis@gmail.com>
2023-04-24 12:48:04 +0300

    refactor: update html according to rule 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.querySelector('.new-task-add-btn');//first button
+var addButton=document.querySelector('.todo-add-btn');//first button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
